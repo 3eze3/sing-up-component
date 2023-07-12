1cadf3df84470ec34a2c459a12c93ab2ba61591a
Ezequiel Cordova <ezekics33@gmail.com>
2023-07-12 18:58:44 -0500

    Add the main to listener

---
+++ build/app/main.js
@@ -0,0 +1,13 @@
+// import { validFormat, back } from "./validForm.mjs";
+import { Form } from "./form.mjs";
+function main() {
+  const button = document.querySelector(".form__btn");
+  const back_button = document.querySelector(".success__button");
+  const objForm = new Form()
+  button.addEventListener("click", e => {
+    e.preventDefault();
+    objForm.showSucessCard()
+  });
+  back_button.addEventListener("click", () => objForm.backLogin())
+}
+main()
