ab8c2b6733556cd0ddf62b1690b59ab09d5536cb
Ezequiel Cordova <ezekics33@gmail.com>
2023-07-12 18:58:06 -0500

    Add the class form

---
+++ build/app/form.mjs
@@ -0,0 +1,32 @@
+export class Form {
+  constructor() {
+    this.email = document.getElementById("email");
+    this.login = document.querySelector('.card__content');
+    this.success = document.querySelector('.success');
+  }
+
+  isValidEmail() {
+    const regexEmail = /\w@(gmail|hotmail|yahoo).com$/gi;
+    return regexEmail.test(this.email.value);
+  }
+
+  handleError() {
+    const messageError = document.querySelector('.form__error');
+    this.email.classList.toggle("form__inputError", !this.isValidEmail());
+    messageError.classList.toggle("form__error--active", !this.isValidEmail())
+  }
+
+  showSucessCard() {
+    if (this.isValidEmail()) {
+      this.success.classList.add("success__show");
+      this.login.classList.add("card__hidden")
+    } else {
+      this.handleError()
+    }
+  }
+
+  backLogin() {
+    this.success.classList.remove('success__show');
+    this.login.classList.remove('card__hidden');
+  }
+}
